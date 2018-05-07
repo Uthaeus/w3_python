de8ec6db788b5a5c19ca4ed36ca4263e5fb892a4
Uthaeus <romanlavery@gmail.com>
2018-05-07 16:18:15 -0600

    Check list, break if specific number

--- 28.py
+++ 28.py
@@ -1,12 +1,18 @@
-test = ['one', 'two', 'three']
+# Write a Python program to print all even numbers from a given numbers list in the same order and stop the printing if any numbers that come after 237 in the sequence.
 
-# print(" ".join(test))
+def checker(l):
+  for x in l:
+    if x % 2 == 0:
+      print(x)
 
-user_in = input("Enter a list of words:\n")
+    if x == 237:
+      break
 
-user_input = user_in.split(', ')
-out = " ".join(user_input)
+numbers = [    
+    386, 462, 47, 418, 907, 344, 236, 375, 823, 566, 597, 978, 328, 615, 953, 345, 
+    399, 162, 758, 219, 918, 237, 412, 566, 826, 248, 866, 950, 626, 949, 687, 217, 
+    815, 67, 104, 58, 512, 24, 892, 894, 767, 553, 81, 379, 843, 831, 445, 742, 717, 
+    958,743, 527
+    ]
 
-print(user_input)
-
-print(out)
+checker(numbers)
