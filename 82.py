5ec06524d51e28e50d147e5a5757b14ac44c505c
Uthaeus <romanlavery@gmail.com>
2018-05-19 08:42:33 -0600

    Write a Python program to calculate the sum over a container.

---
+++ 82.py
@@ -0,0 +1,15 @@
+# Write a Python program to calculate the sum over a container.
+
+
+def summer(l):
+  result  = sum(l)
+  return result
+
+
+t1 = [1, 2, 3]
+t2 = [3, 4, 5]
+t3 = [10, 20, 30, 40]
+
+print(summer(t1))
+print(summer(t2))
+print(summer(t3))
