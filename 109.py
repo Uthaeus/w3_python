ccd01577c26f736d4537c53f349ebda7cc23b01c
Uthaeus <romanlavery@gmail.com>
2018-05-26 19:00:15 -0600

    Write a Python program to check if a number is positive, negative or zero.

---
+++ 109.py
@@ -0,0 +1,17 @@
+# Write a Python program to check if a number is positive, negative or zero.
+
+
+
+def num_value(n):
+  if n == 0:
+    print('Equal to zero')
+  elif n < 0:
+    print('Negative')
+  elif n > 0:
+    print('Positive')
+
+
+
+num_value(4)
+num_value(-34)
+num_value(0)
